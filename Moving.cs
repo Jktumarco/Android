badedf5488b7fc348f9d316ffcc1fb54d4b0d46e
Jktumarco <jktuystremskay@gmail.com>
2021-09-16 02:16:10 +0300

    new

--- Moving.cs
+++ Moving.cs
@@ -11,14 +11,13 @@ public class Moving : MonoBehaviour
     private void FixedUpdate()
     {
             float r = target.transform.rotation.y;
-        //    Vector3 a = transform.position;
+       
             Vector3 b = target.transform.position - new Vector3(0,0,2);
-        //    transform.position = Vector3.MoveTowards(a, b, speed);
-        //    transform.rotation = Quaternion.EulerRotation(0f,Mathf.Lerp(this.transform.rotation.y, r, 4),0f);
+     
         Vector3 a = transform.position;
         
         transform.position = Vector3.Lerp(a, b, speed);
-        //transform.rotation = Quaternion.EulerRotation(0f, Mathf.Lerp(this.transform.rotation.y, r, 4), 0f);
+        
         
 
     }
